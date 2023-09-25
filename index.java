73ebb4fa2b1bd41a810637575bd2a06891a075e4
dCOMMICS <sanwurld@gmail.com>
2023-09-25 19:31:16 +0300

    dynamic connectivity client

--- index.java
+++ index.java
@@ -7,3 +7,16 @@ for (double t = 0.0; true; t = t + dt)
  bodies[i].addForce(bodies[j]);
 
  // 
+
+ public static void main(String[] args)
+{
+ int N = StdIn.readInt();
+ UF uf = new UF(N);
+ while (!StdIn.isEmpty())
+ {
+ int p = StdIn.readInt();
+ int q = StdIn.readInt();
+ if (!uf.connected(p, q))
+ {
+ uf.union(p, q);
+ StdOut.println(p + " " + q);
