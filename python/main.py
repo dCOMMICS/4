614bf3c380e0377b230ea3730fdcd1174c990071
dCOMMICS <sanwurld@gmail.com>
2023-09-29 00:53:16 +0300

    making the surface and the images

--- python/main.py
+++ python/main.py
@@ -8,16 +8,31 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("game one")
 
 WHITE =(255,255,255)
-
+#  changes background color
 def draw_window():
     WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP_IMAGE,(300,100))
     pygame.display.update()
 
+
+FPS = 60
+
+SPACESHIP_WIDTH,SPACESHIP_HEIGHT =55,40
+
+# add some image later 
+YELLOW_SPACESHIP_IMAGE =pygame.image.load(os.path.join())
+
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE,(55,40))
+RED_SPACESHIP = pygame.image.load(os.path.join())
+YELLOW_SPACESHIP_IMAGE =pygame.image.load(os.path.join())
+YELLOW_SPACESHIP_IMAGE =pygame.image.load(os.path.join())
 def main():
-    
+    clock = pygame.time.Clock()
     run = True
     
     while run:
+        
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -29,6 +44,7 @@ def main():
                 if __name__ == '__main__':
                     main()
                 
+                # unfortunately this code is not used. for now.
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     run = False
