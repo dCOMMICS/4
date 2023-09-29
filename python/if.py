9431907acd64e54f865de852f606491fc1d3e4b9
dCOMMICS <sanwurld@gmail.com>
2023-09-29 14:10:02 +0300

    statements

--- python/if.py
+++ python/if.py
@@ -8,3 +8,12 @@ if kjAge > PjAge:
     # statement to be run 
     
     print(kjAge, "is older than", PjAge)
+    
+    #  statement fils de la formula 
+    if kjAge < PjAge:
+        
+        print("fils de joies")
+        
+    elif kjAge > PjAge:
+        
+        print("fils d'enfants")
