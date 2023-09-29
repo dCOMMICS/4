2e402dab6df12974292c7f322db5f72975bea100
dCOMMICS <sanwurld@gmail.com>
2023-09-29 09:43:38 +0300

    this was all about data types in python.

---
+++ python/day3.py
@@ -0,0 +1,47 @@
+# data types in python 
+
+# 1. interger 
+# 2. boolean  TRUE OR FALSE SITUATION
+# 3. float  DECIMALS I.E 16.02
+# 4. complex    this is a mixtures of intergers and strings i.e 14k + 10
+# 5. string   sequences of characters i.e "abcdefghijklmnop"
+
+# interger types
+
+childAge = 17;
+
+print(childAge); # this should print the child age 17
+
+print(type(childAge)) # this should print the data type of childAge that is interger type.
+
+#  boolean
+
+isMarried = True;
+
+print(isMarried);   # this should   be TRUE
+
+print(type(isMarried)) # this should print the  data type of isMarried that is  boolean type.
+
+# float 
+
+height = 1.80;
+
+print(height); # this should print the height 1.80
+
+print(type(height)) # this should print the data type of height that is float type.
+
+# complex     
+
+complexNumber = 14 + 10j;
+
+print(complexNumber); # this should print the complex number 14 + 10j
+
+print(type(complexNumber)) # this should print the data type of complexNumber that is complex type.
+
+# string
+
+name = "<NAME>";
+
+print(name); # this should print the name "<NAME>"
+
+print(type(name)) # this should print the data type of name that is string type.
