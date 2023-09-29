d92f0555ecd5155b95429fd77008a23a4b4f6099
dCOMMICS <sanwurld@gmail.com>
2023-09-29 10:37:19 +0300

    unit conversion

--- python/day3.py
+++ python/day3.py
@@ -65,5 +65,11 @@ def myfunc():
     from datetime import datetime
     
     day = datetime.today();
+    parsecs = 11
+
+lightyears = parsecs * 3.26
+
+print(str(parsecs)+ " parsecs is " + str(lightyears)+"lightyears") 
+    
     
     
