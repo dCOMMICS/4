6eda3f15ca31362cf610924acd1ae109bf4fb78a
dCOMMICS <sanwurld@gmail.com>
2023-09-29 11:00:20 +0300

    creating reusable applications

--- python/day3.py
+++ python/day3.py
@@ -99,6 +99,11 @@ print(int(first_name + " " + last_name))
 
 print(int(first_name) + int(last_name))
 
+parsecs_input = input("Input number of parsecs:")
+parsecs = int(parsecs_input)
+lightyears = 3.26156 * parsecs
+print(parsecs_input + "parsecs is " + str(lightyears) + "lightyears")
+
 
     
     
