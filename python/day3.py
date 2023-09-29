e8c1078c856d746c3621a4473ed319374da5a29b
dCOMMICS <sanwurld@gmail.com>
2023-09-29 10:38:58 +0300

    cli input

--- python/day3.py
+++ python/day3.py
@@ -70,6 +70,12 @@ def myfunc():
 lightyears = parsecs * 3.26
 
 print(str(parsecs)+ " parsecs is " + str(lightyears)+"lightyears") 
+
+import sys
+
+print(sys.argv)
+print(sys.argv[0]) # program name
+print(sys.argv[1]) # first arg
     
     
     
