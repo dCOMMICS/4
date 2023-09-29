ab5e3b601586c76c32c6d6c5aff6e25c86401aec
dCOMMICS <sanwurld@gmail.com>
2023-09-29 14:20:42 +0300

    why is this taking slow to read

--- python/new.py
+++ python/new.py
@@ -22,3 +22,12 @@ else:
             
         else:
             print("something went wrong")
+            
+            
+            object_size = 10;
+if object_size > 5:
+    print("we need to keep an eye on this object")
+
+else:
+
+        print('Object poses no threat')
